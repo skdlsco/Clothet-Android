8ed5668bb1ee2240bdcaeba574f2fe8035542f10
Eka <nicnic1234@naver.com>
2017-09-01 06:47:14 +0900

    first commit

---
+++ app/src/main/java/kr/wonjun/clothet/ClosetActivity.kt
@@ -0,0 +1,13 @@
+package com.clothet
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.wonjun.clothet.R
+
+class ClosetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_closet)
+    }
+}
